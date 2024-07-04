1755754040813920be2550cded32414c9dd77d12
AlichaXD <dheriap248@gmail.com>
2024-07-04 07:48:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ const profileSettings = {
   location: "Bekasi, Jawa Barat, Indonesia", // Lokasi
   backgroundImage: "background.jpg", // Latar belakang halaman
   links: [
-    { title: "Tiktok", url: "https://tiktok.com/@ghanzxxyz2009" },
+    { title: "Tiktok", url: "tiktok.com/@ghanzxxyz2009" },
     { title: "Instagram", url: "https://www.instagram.com/ghanz_anak_sholeh?igsh=MTNqYmZ5ZGlnZTJwYw==" },
     { title: "Whatsapp", url: "https://wa.me/6289654782544" },
     { title: "Saweria", url: "https://saweria.co/" },
